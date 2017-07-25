0544d0a7ab42eb372c82ac6a09695798f40681d9
Magwit <magnushakon@hotmail.com>
2017-07-25 20:28:24 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Hei");
+
+
+
+
+// document.getElementById("hamburger-icon").addEventListener("click", function(){
+//     document.getElementsByTagName("li").style.display = "none";
+// });
+
+
+// TODO: Use this model to create the effect. https://codepen.io/escherina/pen/pyxYqz Det är tid att plugga javascript igen?
