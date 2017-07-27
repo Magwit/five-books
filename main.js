3323e5c01bade2ef9ba84fedc959af7d32d906e3
Magwit <magnushakon@hotmail.com>
2017-07-27 05:29:14 +0300

    adjust the scrollTop values to height of header on desktop and adapt hide menu behaviour to only apply to mobile/tablet

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 // Navigation with offset (for desktop) and scroll
 
 $(function() {
-  console.log("Hei");
 
-  var off = 0;
+  var off;
+  var headerHheight = $(".header").height();
 
-  // defauult
-  // if desktop display block
-  if( $(window).width() >= 960  ) {
-    // $(".menu").css("display","block");
-    // toggleClass to block-display
-    //
-    off = 60;
-
+  // set default for desktop
+  if ($(window).width() >= 960) {
+    off = headerHheight;
+  } else {
+    // set defaults for mobile and tablet
+    $(".menu").toggleClass("none-display");
+    off = 0;
   }
 
-  // click hamburger icon to display menu
+  // click hamburger icon or section to display menu
 
-  $(".hamburger-icon").on('click',function() {
-    console.log("burgertime!");
+  $(".hamburger-icon").on('click', function() {
     $(".menu").toggleClass("none-display");
     // $(".menu").css("display", "block");
   })
@@ -28,44 +26,27 @@ $(function() {
   $("a[href^='#']").on('click', function(e) {
     // prevent default anchor click behavior
     e.preventDefault();
-
-    // hide the menu for mobile and tablet.
-
-    var off = 0;
-
-    if ($(window).width() < 401) {
-      console.log("mobile");
-      // $('.menu').css("display", "none");
-      // set a variable that I can use in animate
-    } else if ($(window).width() < 959 && $(window).width() > 400) {
-      console.log('tablet');
-      // $('.menu').css("display", "none");
-      // set a variable that I can use in animate
-    } else {
-      console.log('desktop');
-      // set a variable that I can use in animate
-      off = 60;
+    // hide menu for mobile and tablet
+    if ($(window).width() < 960) {
+      $(".menu").toggleClass("none-display");
     }
-
-    /*
-    var poxel = 60;
-    var poxel = $('.menu').height();
-    console.log(poxel);
-    */
-
     // animate
     $('html, body').animate({
       scrollTop: $(this.hash).offset().top - off
     }, 300, function() {});
   });
-  console.log("the end");
+
+
+
+
 });
 
 
 // DONE mobile and tablet: Open close nav.
 // DONE: conditional media query hide for mobie and rtablet.
-// DONE basic hamburget toggle for mobile and tablet
-// TODO: troubleshoot and polish the hamburger and nav
+// DONE: basic hamburget toggle for mobile and tablet
+// DONE: troubleshoot and polish the hamburger and nav
+// TODO: adjust the scrollTop values to height of header on desktop and adapt hide menu behaviour to only apply to mobile/tablet
 
 // Next if needed: Conditional offset for mobile and tablet..  https://stackoverflow.com/questions/10732690/offsetting-an-html-anchor-to-adjust-for-fixed-header*/
 
