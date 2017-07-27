c19f7a6fa2882ca4a52721d2d27ddfaa73436989
Magwit <magnushakon@hotmail.com>
2017-07-27 10:53:14 +0300

    add font and image  attributions to footer

--- main.js
+++ main.js
@@ -48,6 +48,7 @@ $(function() {
 // DONE: troubleshoot and polish the hamburger and nav
 // DONE: adjust the scrollTop values to height of header on desktop and adapt hide menu behaviour to only apply to mobile/tablet
 // DONE: simple discreet menu hover effect.. bigger title header
+// TODO: attributions
 
 // Next if needed: Conditional offset for mobile and tablet..  https://stackoverflow.com/questions/10732690/offsetting-an-html-anchor-to-adjust-for-fixed-header*/
 
